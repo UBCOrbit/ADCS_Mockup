5482298dcd678359a6d8339026bad5fdfb1df3cb
Jasper Chan <jasperchan515@gmail.com>
2019-06-12 02:42:10 -0700

    Replace debug print hack with dedicated debug task

---
+++ src/orbit_debug.c
@@ -0,0 +1,43 @@
+#include "orbit_debug.h"
+
+void initDebugTask() {
+    sciInit();
+    debugQueueHandle = xQueueCreate(10, sizeof(DebugMsg*));
+
+    if(debugQueueHandle == NULL)
+        for(;;);
+
+    if(xTaskCreate(
+                debugTask,
+                "Debug",
+                configMINIMAL_STACK_SIZE,
+                NULL, 1, NULL)
+            != pdTRUE)
+        for(;;);
+}
+
+void debugTask(void* p) {
+    DebugMsg* m;
+    for(;;) {
+        if(xQueueReceive(debugQueueHandle, &m, 100)) {
+            if(m != NULL) {
+                sciSend(scilinREG, strlen(m->msg), (uint8*)m->msg);
+                sciSend(scilinREG, 2, "\r\n");
+                if(m->dynamic) {
+                    free(m->msg);
+                    free(m);
+                }
+            }
+        }
+    }
+}
+
+uint8_t debugPrint(DebugMsg **p) {
+    uint8_t i;
+    for(i=0; i<ORBIT_DEBUG_RETRIES; i++)
+        if(xQueueSend(debugQueueHandle, (void*)p, 100))
+            return ORBIT_DEBUG_SUCCESS;
+    return ORBIT_DEBUG_FAIL;
+}
+
+
